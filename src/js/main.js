ac2c2e795dca53fde67bcd25b85a7d0fdf35a07a
Samantha Davis <samantha.shea.davis@gmail.com>
2016-10-13 15:01:59 -0400

    added google api call

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
-var biscuitCity = {
+var googleUser = {
+    apiKey: 'AIzaSyCU2Fp6SIYAD--ig0VwHGaRA-b9yS6M27k'
+};
+var twitterUser = {
+    apiKey: null
+};
+
+var buildMe = {
 
     generateTemplate: function(pageTemplate) {
         var source = $(pageTemplate).html();
@@ -8,7 +15,7 @@ var biscuitCity = {
         };
         var html = template(context);
         $('#templates-container').fadeOut('fast', function() {
-             $('#container').html('').prepend(html).fadeIn();
+            $('#templates-container').html('').prepend(html).fadeIn();
         });
         //
         // $('#templates-container').fadeOut('fast', function() {
@@ -18,22 +25,78 @@ var biscuitCity = {
     triggerTemplate: function() {
 
         $('#container').on('click', '.home', function(event) {
-            biscuitCity.generateTemplate('#home-template');
+            buildMe.generateTemplate('#home-template');
         });
         $('#container').on('click', '.social', function(event) {
-            biscuitCity.generateTemplate('#social-template');
+            buildMe.generateTemplate('#social-template');
             console.log('social');
         });
         $('#container').on('click', '.maps', function(event) {
-            biscuitCity.generateTemplate('#maps-template');
+            buildMe.generateTemplate('#maps-template');
             console.log('maps');
         });
         $('#container').on('click', '.recipe', function(event) {
-            biscuitCity.generateTemplate('#recipe-template');
+            buildMe.generateTemplate('#recipe-template');
             console.log('recipes');
         });
     },
 };
+var map;
+var infowindow;
 
-biscuitCity.triggerTemplate();
-biscuitCity.generateTemplate('#home-template');
+function initMap() {
+    var rtp = {
+        lat: 35.914224,
+        lng: 78.865230
+    };
+
+    map = new google.maps.Map($('.maps-container'), {
+        center: rtp,
+        zoom: 25
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+            location: rtp,
+            radius: 500,
+            type: ['Red Lobster']
+        },
+        callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+  infowindow.setContent(place.name);
+  infowindow.open(map, this);
+});
+}
+
+$.ajax({
+  url: 'https://maps.googleapis.com/maps/api/js?key=' + googleUser.apiKey + '&libraries=places&callback=initMap',
+  type: 'GET',
+  dataType: 'jsonp'
+});
+
+// $.get('https://maps.googleapis.com/maps/api/js?key=' + googleUser.apiKey + '&libraries=places&callback=initMap');
+// add error handling here
+
+
+
+buildMe.triggerTemplate();
+buildMe.generateTemplate('#home-template');
+// twitter();
